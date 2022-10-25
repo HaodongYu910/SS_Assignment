359c848d81f127f30bb86b149562040905ec4d8c
haodong <haodong.yu@hotmail.com>
2022-10-25 14:48:43 +0100

    123

---
+++ main.py
@@ -0,0 +1,14 @@
+# main function, program entrance
+
+
+def print_name():
+    print("what's your name???")
+
+if __name__ == "__main__":
+    print_name()
+    
+    
+def test():
+    print("test")
+    
+
