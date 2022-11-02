3d2d79ffa67719244e315c492e174eff30c07b4e
haodong <haodong.yu@hotmail.com>
2022-11-02 22:43:27 +0000

    refector main function

--- Function/main.py
+++ Function/main.py
@@ -1,9 +1,9 @@
 # main function, program entrance
+import ttkbootstrap as ttk
 
+from GUI.welcom_page import basedesk
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     root = ttk.Window()
     basedesk(root)
     root.mainloop()
-
--- README.md
+++ README.md
@@ -4,5 +4,5 @@ Software and System assignment project
 To start：
   1. 使用pycharm打开project文件夹
   2. use "pip3 install -r requirement.txt" 安装所需要的所有plugin
-  3. 找到GUI > welcome_page > 点击main函数运行
+  3. 找到Function > main.py > 点击main函数运行
 
