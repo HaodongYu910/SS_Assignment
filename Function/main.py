0a9164acf957b5fd3c466f0b702194a0624afb49
haodong <haodong.yu@hotmail.com>
2022-12-13 01:03:28 +0000

    add init of game

--- Function/main.py
+++ Function/main.py
@@ -1,6 +1,7 @@
 # main function, program entrance
 import ttkbootstrap as ttk
 
+from Function.gl import game
 from GUI import start_page, select_page
 from GUI.welcom_page import basedesk
 
@@ -8,6 +9,7 @@ if __name__ == '__main__':
     startPage = start_page.Snake_and_Ladders()
     startPage.run_game()
     while True:
+        game.__init__()
         root = ttk.Window()
         basedesk(root)
         root.mainloop()
